3b5268d00b31bbce6434ae3644a098a08e0fdb37
florent champigny <fchampigny@xebia.fr>
2015-12-02 13:58:44 +0100

    added empty configuration app

---
+++ app/src/main/java/fr/xebia/mywatchface/MyConfigurationActivity.java
@@ -0,0 +1,15 @@
+package fr.xebia.mywatchface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by florentchampigny on 02/12/2015.
+ */
+public class MyConfigurationActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration_activity);
+    }
+}
